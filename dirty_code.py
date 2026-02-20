63e501cc34443f362d6f0aa98b598259ef3bdda1
Lavanya N M <lavanyanm75@gmail.com>
2026-02-20 07:01:30 +0530

    Initial mock commit

--- dirty_code.py
+++ dirty_code.py
@@ -1,2 +1,5 @@
+import os
+import sys
+
 def do_nothing():
     pass
--- test_logic.py
+++ test_logic.py
@@ -1,4 +1,4 @@
 import unittest
 class TestMath(unittest.TestCase):
     def test_sum(self):
-        assert 1 + 1 == 2, 'Math is broken'
+        assert 1 + 1 == 3, 'Math is broken'
